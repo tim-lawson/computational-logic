fc64da722158189bf847bb45db83fe0177d91c2f
Tim Lawson <qr23940@bristol.ac.uk>
2024-05-01 14:21:07 +0100

    refactor tests

--- prolog/cli_test.py
+++ prolog/cli_test.py
@@ -1,212 +1,192 @@
-"""Test the CLI with input-output pairs."""
+# pylint: disable=missing-function-docstring, missing-module-docstring, missing-class-docstring
 
 import subprocess
 
 
-def cli(*args: tuple[str, str]):
-    """Test the CLI with the given input-output pairs."""
+class Cli:
+    Remember = "I will remember that."
 
-    process = subprocess.Popen(
-        ["./cli"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True
-    )
+    def __init__(self):
+        self.process = subprocess.Popen(
+            ["./cli"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True
+        )
 
-    assert process.stdin is not None
-    assert process.stdout is not None
+    def __call__(self, value: str):
+        assert self.process.stdin is not None
+        assert self.process.stdout is not None
 
-    for test_input, test_output in args:
-        process.stdin.write(f"{test_input}\n")
-        process.stdin.flush()
+        self.process.stdout.flush()
+        self.process.stdin.write(f"{value}\n")
+        self.process.stdin.flush()
+        return self.process.stdout.readline().rstrip("\n")
 
-        stdout = process.stdout.readline()
-        process.stdout.flush()
-        assert stdout == f"{test_output}\n"
+    def __del__(self):
+        assert self.process.stdin is not None
+        assert self.process.stdout is not None
 
-    process.communicate()
-
-
-REMEMBER = "I will remember that."
+        self("stop")
+        self.process.terminate()
 
 
 def test_implication():
-    """Test the CLI with implication rules."""
-
-    cli(
-        ("alice is human", REMEMBER),
-        ("every human is mortal", REMEMBER),
-        ("is alice mortal", "alice is mortal"),
-        (
-            "explain why alice is mortal",
-            "alice is human, every human is mortal, therefore alice is mortal",
-        ),
-        ("is alice not mortal", "alice is mortal"),
-        (
-            "explain why alice is not mortal",
-            "alice is human, every human is mortal, therefore alice is mortal",
-        ),
+    cli = Cli()
+    assert cli("alice is human") == Cli.Remember
+    assert cli("every human is mortal") == Cli.Remember
+    assert cli("is alice mortal") == "alice is mortal"
+    assert (
+        cli("explain why alice is mortal")
+        == "alice is human, every human is mortal, therefore alice is mortal"
+    )
+    assert cli("is alice not mortal") == "alice is mortal"
+    assert (
+        cli("explain why alice is not mortal")
+        == "alice is human, every human is mortal, therefore alice is mortal"
     )
 
 
 def test_negation():
-    """Test the CLI with negation rules."""
-
-    cli(
-        ("every teacher is happy", REMEMBER),
-        ("donald is not happy", REMEMBER),
-        ("is donald a teacher", "donald is not a teacher"),
-        (
-            "explain why donald is a teacher",
-            "donald is not happy, every teacher is happy, therefore donald is not a teacher",
-        ),
-        ("is donald not a teacher", "donald is not a teacher"),
-        (
-            "explain why donald is not a teacher",
-            "donald is not happy, every teacher is happy, therefore donald is not a teacher",
-        ),
+    cli = Cli()
+    assert cli("every teacher is happy") == Cli.Remember
+    assert cli("donald is not happy") == Cli.Remember
+    assert cli("is donald a teacher") == "donald is not a teacher"
+    explanation = (
+        "donald is not happy, every teacher is happy, therefore donald is not a teacher"
     )
+    assert cli("explain why donald is a teacher") == explanation
+    assert cli("is donald not a teacher") == "donald is not a teacher"
+    assert cli("explain why donald is not a teacher") == explanation
 
 
 def test_negation_simple():
-    """Test the CLI with negation rules."""
+    cli = Cli()
+    assert cli("pixie is not blue") == Cli.Remember
+    assert cli("is pixie blue") == "pixie is not blue"
+    assert cli("is pixie not blue") == "pixie is not blue"
 
-    cli(
-        ("pixie is not blue", REMEMBER),
-        ("is pixie blue", "pixie is not blue"),
-    )
+    cli = Cli()
+    assert cli("alice does not fly") == Cli.Remember
+    assert cli("does alice fly") == "alice does not fly"
+    assert cli("does alice not fly") == "alice does not fly"
 
 
 def test_disjunction():
-    """Test the CLI with disjunction rules."""
-
-    cli(
-        ("pixie is a pixel", REMEMBER),
-        ("every pixel is red or blue", REMEMBER),
-        ("pixie is red", REMEMBER),
-        ("is pixie blue", "pixie is not blue"),
-        (
-            "explain why pixie is blue",
-            "every pixel is red or is blue, pixie is a pixel, pixie is red, therefore pixie is not blue",
-        ),
-        ("is pixie not blue", "pixie is not blue"),
-        (
-            "explain why pixie is not blue",
-            "every pixel is red or is blue, pixie is a pixel, pixie is red, therefore pixie is not blue",
-        ),
+    cli = Cli()
+    assert cli("pixie is a pixel") == Cli.Remember
+    assert cli("every pixel is red or blue") == Cli.Remember
+    assert cli("pixie is red") == Cli.Remember
+    assert cli("is pixie blue") == "pixie is not blue"
+    assert (
+        cli("explain why pixie is blue")
+        == "every pixel is red or is blue, pixie is a pixel, pixie is red, therefore pixie is not blue"
+    )
+    assert cli("is pixie not blue") == "pixie is not blue"
+    assert (
+        cli("explain why pixie is not blue")
+        == "every pixel is red or is blue, pixie is a pixel, pixie is red, therefore pixie is not blue"
     )
 
-    cli(
-        ("pixie is a pixel", REMEMBER),
-        ("every pixel is red or blue", REMEMBER),
-        ("pixie is not red", REMEMBER),
-        ("is pixie blue", "pixie is blue"),
-        (
-            "explain why pixie is blue",
-            "every pixel is red or is blue, pixie is a pixel, pixie is not red, therefore pixie is blue",
-        ),
-        ("is pixie not blue", "pixie is blue"),
-        (
-            "explain why pixie is not blue",
-            "every pixel is red or is blue, pixie is a pixel, pixie is not red, therefore pixie is blue",
-        ),
+    cli = Cli()
+    assert cli("pixie is a pixel") == Cli.Remember
+    assert cli("every pixel is red or blue") == Cli.Remember
+    assert cli("pixie is not red") == Cli.Remember
+    assert cli("is pixie blue") == "pixie is blue"
+    assert (
+        cli("explain why pixie is blue")
+        == "every pixel is red or is blue, pixie is a pixel, pixie is not red, therefore pixie is blue"
+    )
+    assert cli("is pixie not blue") == "pixie is blue"
+    assert (
+        cli("explain why pixie is not blue")
+        == "every pixel is red or is blue, pixie is a pixel, pixie is not red, therefore pixie is blue"
     )
 
 
 def test_disjunction_three_term():
-    """Test the CLI with disjunction rules."""
     # Question is middle term
-    cli(
-        ("pixie is a pixel", REMEMBER),
-        ("every pixel is red green or blue", REMEMBER),
-        ("pixie is not red", REMEMBER),
-        ("pixie is not blue", REMEMBER),
-        ("is pixie green", "pixie is green"),
-        (
-            "explain why pixie is green",
-            "every pixel is red is green or is blue, pixie is a pixel, pixie is not red, pixie is not blue, therefore pixie is green",
-        ),
+    cli = Cli()
+    assert cli("pixie is a pixel") == Cli.Remember
+    assert cli("every pixel is red green or blue") == Cli.Remember
+    assert cli("pixie is not red") == Cli.Remember
+    assert cli("pixie is not blue") == Cli.Remember
+    assert cli("is pixie green") == "pixie is green"
+    assert (
+        cli("explain why pixie is green")
+        == "every pixel is red is green or is blue, pixie is a pixel, pixie is not red, pixie is not blue, therefore pixie is green"
     )
+
     # Question is first term
-    cli(
-        ("pixie is a pixel", REMEMBER),
-        ("every pixel is red green or blue", REMEMBER),
-        ("pixie is not green", REMEMBER),
-        ("pixie is not blue", REMEMBER),
-        ("is pixie red", "pixie is red"),
-        (
-            "explain why pixie is red",
-            "every pixel is red is green or is blue, pixie is a pixel, pixie is not green, pixie is not blue, therefore pixie is red",
-        ),
+    cli = Cli()
+    assert cli("pixie is a pixel") == Cli.Remember
+    assert cli("every pixel is red green or blue") == Cli.Remember
+    assert cli("pixie is not green") == Cli.Remember
+    assert cli("pixie is not blue") == Cli.Remember
+    assert cli("is pixie red") == "pixie is red"
+    assert (
+        cli("explain why pixie is red")
+        == "every pixel is red is green or is blue, pixie is a pixel, pixie is not green, pixie is not blue, therefore pixie is red"
     )
 
     # Question is last term
-    cli(
-        ("pixie is a pixel", REMEMBER),
-        ("every pixel is red green or blue", REMEMBER),
-        ("pixie is not red", REMEMBER),
-        ("pixie is not green", REMEMBER),
-        ("is pixie blue", "pixie is blue"),
-        (
-            "explain why pixie is blue",
-            "every pixel is red is green or is blue, pixie is a pixel, pixie is not red, pixie is not green, therefore pixie is blue",
-        ),
+    cli = Cli()
+    assert cli("pixie is a pixel") == Cli.Remember
+    assert cli("every pixel is red green or blue") == Cli.Remember
+    assert cli("pixie is not red") == Cli.Remember
+    assert cli("pixie is not green") == Cli.Remember
+    # TODO: this fails
+    assert cli("is pixie blue") == "pixie is blue"
+    assert (
+        cli("explain why pixie is blue")
+        == "every pixel is red is green or is blue, pixie is a pixel, pixie is not red, pixie is not green, therefore pixie is blue"
     )
 
 
 def test_disjunctive_questions():
-    """Test the CLI with disjunctive questions."""
-    cli(
-        ("pixie is red", REMEMBER),
-        ("is pixie red or blue", "pixie is red or is blue"),
-        (
-            "explain why pixie is red or blue",
-            "pixie is red, therefore pixie is red or is blue",
-        ),
-        ("is pixie blue or red", "pixie is blue or is red"),
-        (
-            "explain why pixie is blue or red",
-            "pixie is red, therefore pixie is blue or is red",
-        ),
+    cli = Cli()
+    assert cli("pixie is red") == Cli.Remember
+    assert cli("is pixie red or blue") == "pixie is red or is blue"
+    assert (
+        cli("explain why pixie is red or blue")
+        == "pixie is red, therefore pixie is red or is blue"
+    )
+    assert cli("is pixie blue or red") == "pixie is blue or is red"
+    assert (
+        cli("explain why pixie is blue or red")
+        == "pixie is red, therefore pixie is blue or is red"
     )
 
 
 def test_conjunction():
-    """Test the CLI with conjunction rules."""
-
-    cli(
-        ("bob is human and mortal", REMEMBER),
-        ("is bob human", "bob is human"),
-        ("is bob mortal", "bob is mortal"),
-    )
+    cli = Cli()
+    assert cli("bob is human and mortal") == Cli.Remember
+    assert cli("is bob human") == "bob is human"
+    assert cli("is bob mortal") == "bob is mortal"
 
 
 def test_conjunction_three_term():
-    """Test the CLI with conjunction rules."""
     # Question is middle term
-    cli(
-        ("bob is human mortal and does not fly", REMEMBER),
-        ("is bob human", "bob is human"),
-        ("is bob mortal", "bob is mortal"),
-        ("does bob fly", "bob does not fly"),
-    )
+    cli = Cli()
+    assert cli("bob is human mortal and does not fly") == Cli.Remember
+    assert cli("is bob human") == "bob is human"
+    assert cli("is bob mortal") == "bob is mortal"
+
+    # TODO: this fails
+    assert cli("does bob fly") == "bob does not fly"
 
 
 def test_conjunction_questions():
-    """Test the CLI with conjunctive questions."""
-    cli(
-        ("bob is human mortal and does not fly", REMEMBER),
-        ("is bob mortal and human", "bob is mortal and is human"),
-    )
+    cli = Cli()
+    assert cli("bob is human mortal and does not fly") == Cli.Remember
+    assert cli("is bob mortal and human") == "bob is mortal and is human"
 
 
 def test_default():
-    """Test the CLI with default rules."""
-    cli(
-        ("most birds fly", REMEMBER),
-        ("alice is a bird", REMEMBER),
-        ("does alice fly", "it is likely that alice flies"),
-        (
-            "explain why alice flies",
-            "alice is a bird, most birds fly, therefore it is likely that alice flies",
-        ),
+    cli = Cli()
+    assert cli("most birds fly") == Cli.Remember
+    assert cli("alice is a bird") == Cli.Remember
+    assert cli("does alice fly") == "it is likely that alice flies"
+    assert (
+        cli("explain why alice flies")
+        == "alice is a bird, most birds fly, therefore it is likely that alice flies"
     )
 
 
